648e91e08e1808d5a1e88203b7e59c1e7e54cbe9
Juan Galarce <j6alarce@gmail.com>
2022-08-26 15:45:30 -0400

    Se rellena el select con los datos del state categories, se añade un button al form

--- src/components/Formulary.jsx
+++ src/components/Formulary.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Button, Form, Row, Col } from "react-bootstrap";
+import useCategories from "../hooks/useCategories";
 
 const Formulary = () => {
+  const { categories } = useCategories();
+
   return (
     <Form>
       <Row>
         <Col md={6}>
-          <Form.Group>
+          <Form.Group className="mb-3">
             <Form.Label htmlFor="name">Nombre de Cocktail </Form.Label>
             <Form.Control
               id="name"
@@ -17,12 +20,24 @@ const Formulary = () => {
           </Form.Group>
         </Col>
         <Col md={6}>
-          <Form.Group>
+          <Form.Group className="mb-3">
             <Form.Label htmlFor="category">Categoria Cocktails </Form.Label>
             <Form.Select id="category" name="category">
               <option>-- Selecciona Categoria --</option>
+              {categories.map((category) => (
+                <option key={category.strCategory}>
+                  {category.strCategory}
+                </option>
+              ))}
             </Form.Select>
           </Form.Group>
+        </Col>
+      </Row>
+      <Row className="justify-content-end">
+        <Col md={3}>
+          <Button variant="danger" className="text-uppercase w-100">
+            Buscar Cocktail
+          </Button>
         </Col>
       </Row>
     </Form>
