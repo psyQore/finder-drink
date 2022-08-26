1466e1568e2fa8c5cda9cccdc8ec07fc5f1c3779
Juan Galarce <j6alarce@gmail.com>
2022-08-26 09:56:48 -0400

    Se crea el context y hook para las categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import { Container } from 'react-bootstrap';
+import { Container } from "react-bootstrap";
 import Formulary from "./components/Formulary";
+import { CategoriesProvider } from "./context/CategoryProvider";
 
 function App() {
   return (
-    <>
-      <header className="py-5">
-        <h1>Buscador de Cocktails </h1>
-      </header>
+    <CategoriesProvider>
+      <>
+        <header className="py-5">
+          <h1>Buscador de Cocktails </h1>
+        </header>
 
-      <Container className="mt-5">
-        <Formulary />
-      </Container>
-    </>
+        <Container className="mt-5">
+          <Formulary />
+        </Container>
+      </>
+    </CategoriesProvider>
   );
 }
 
