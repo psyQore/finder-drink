3de27ec022168e9d70f88f04201a629735b8b38e
Juan Galarce <j6alarce@gmail.com>
2022-08-28 11:37:38 -0400

    Se crea la logica para filtrar cada ingrediente y cantidad y se muestra en el modal

--- src/components/ModalDrink.jsx
+++ src/components/ModalDrink.jsx
@@ -3,11 +3,40 @@ import { Modal, Image } from "react-bootstrap";
 import useDrinks from "../hooks/useDrinks";
 
 const ModalDrink = () => {
-  const { modal, handleModalClick } = useDrinks();
+  const { modal, handleModalClick, recipe } = useDrinks();
+
+  const showIngredients = () => {
+    let ingredients = [];
+
+    for (let i = 1; i < 16; i++) {
+      if (recipe[`strIngredient${i}`]) {
+        ingredients.push(
+          <li>
+            {recipe[`strIngredient${i}`]} {recipe[`strMeasure${i}`]}
+          </li>
+        );
+      }
+    }
+    return ingredients;
+  };
 
   return (
     <Modal show={modal} onHide={handleModalClick}>
-      <Modal.Body>Cuerpo</Modal.Body>
+      <Image
+        src={recipe.strDrinkThumb}
+        alt={`Imagen receta ${recipe.strDrink}`}
+      />
+      <Modal.Header>
+        <Modal.Title>{recipe.strDrink}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="p-3">
+          <h2>Intrucciones</h2>
+          {recipe.strInstructions}
+          <h2>Ingredientes y Cantidades</h2>
+          {showIngredients()}
+        </div>
+      </Modal.Body>
     </Modal>
   );
 };
